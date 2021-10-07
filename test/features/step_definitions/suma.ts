3976efef44d0be15c0f03faaa38b5d2fedb8e912
Alvaro Muñoz Garcia <amgarcia@arsys.es>
2021-10-07 13:50:23 +0200

    Suma example with cucumber. Use 'this' to store information

--- test/features/step_definitions/suma.ts
+++ test/features/step_definitions/suma.ts
@@ -3,71 +3,66 @@ import { Given, When, Then } from "@cucumber/cucumber";
 
 import { Calculadora } from "./../../../src/calculadora";
 
-let resultado : number;
-let number1   : number;
-let number2   : number;
-let exception : Error;
+Given( "Dados dos numeros 1 y 2", function () {
 
-Given( "Dados dos numeros 1 y 2", () => {
-
-    number1 = 1;
-    number2 = 2;
+    this.number1 = 1;
+    this.number2 = 2;
 
 } );
 
-Given( "Dados dos numeros -1 y 2", () => {
+Given( "Dados dos numeros -1 y 2", function () {
 
-    number1 = -1;
-    number2 = 2;
-
-} );
-
-Given( "Dados dos numeros 1 y -2", () => {
-
-    number1 = 1;
-    number2 = -2;
+    this.number1 = -1;
+    this.number2 = 2;
 
 } );
 
-Given( "Dados dos numeros -1 y -2", () => {
+Given( "Dados dos numeros 1 y -2", function () {
 
-    number1 = -1;
-    number2 = -2;
+    this.number1 = 1;
+    this.number2 = -2;
 
 } );
 
-When( "Cuando los sumo", () => {
+Given( "Dados dos numeros -1 y -2", function () {
+
+    this.number1 = -1;
+    this.number2 = -2;
+
+} );
+
+When( "Cuando los sumo", function () {
 
     try {
 
-        resultado = Calculadora.suma( number1, number2 );
-        exception = null;
+        this.resultado = Calculadora.suma( this.number1, this.number2 );
+        this.exception = null;
 
     } catch ( error ) {
-        exception = error;
+        this.exception = error;
     }
 
 } );
 
-Then( "La suma es 3", () => {
-    assert.equal( resultado, 3 );
+Then( "La suma es 3", function () {
+    assert.equal( this.resultado, 3 );
 } );
 
-Then( "Obtengo una excepcion", () => {
-    assert.isDefined( exception);
+Then( "Obtengo una excepcion", function () {
+    assert.isDefined( this.exception);
 } );
 
 /* ------------------- */
 
-Given( "X - Dados dos numeros {int} y {int}", ( a : number, b : number ) => {
+Given( "X - Dados dos numeros {int} y {int}", function ( a : number, b : number ) {
 
-    number1 = a;
-    number2 = b;
+    this.number1 = a;
+    this.number2 = b;
 
 } );
 
-Then( "X - La suma es {int}", ( resultadoEsperado : number ) => {
-    assert.equal( resultado, resultadoEsperado );
+Then( "X - La suma es {int}", function ( resultadoEsperado : number ) {
+    assert.equal( this.resultado, resultadoEsperado );
 } );
 
 /* ------------------- */
