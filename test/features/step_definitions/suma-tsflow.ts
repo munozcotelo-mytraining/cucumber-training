adbc3bfc9a5fa87b65a567c73d4a857c2e5f7076
Alvaro Muñoz Garcia <amgarcia@arsys.es>
2021-10-08 10:49:55 +0200

    Usando ts-flow

---
+++ test/features/step_definitions/suma-tsflow.ts
@@ -0,0 +1,64 @@
+import { assert }  from "chai";
+import { binding } from "cucumber-tsflow";
+import { given }   from "cucumber-tsflow";
+import { then }    from "cucumber-tsflow";
+import { when }    from "cucumber-tsflow";
+import { before }  from "cucumber-tsflow";
+import { after }   from "cucumber-tsflow";
+
+import { Calculadora } from "./../../../src/calculadora";
+
+console.info( Calculadora );
+
+@binding()
+class SumaTest {
+
+    private number1   : number;
+    private number2   : number;
+    private resultado : number;
+    private exception : Error;
+
+    /* Cualquiera de las dos vale */
+    @given( "TsFlow Dados dos numeros {int} y {int}" )
+    // @given( /TsFlow Dados dos numeros (.*) y (.*)/ )
+    public settingNumbers(  a : number, b : number ) : void {
+
+        const me : SumaTest = this;
+
+        me.number1 = Number( a );
+        me.number2 = Number( b );
+
+    };
+
+    /* Cualquiera de las dos vale */
+    // @when( /^TsFlow Cuando los sumo$/ )
+    @when( "TsFlow Cuando los sumo" )
+    public doSuma() : void {
+
+        const me : SumaTest = this;
+
+        try {
+
+            me.resultado = Calculadora.suma( me.number1, me.number2 );
+            me.exception = null;
+
+        } catch ( error ) {
+            me.exception = error;
+        }
+
+    }
+
+    /* Cualquiera de las dos vale */
+    @then( "TsFlow La suma es {int}" )
+    // @then( /TsFlow La suma es (.*)/ )
+    public checkSuma( resultadoEsperado : number ) : void {
+
+        const me : SumaTest = this;
+
+        assert.equal( this.resultado, resultadoEsperado );
+
+    }
+
+}
+
+export { SumaTest };
---
+++ test/features/suma-tsflow.feature
@@ -0,0 +1,16 @@
+Feature: Suma
+    Quiero comprobar que la calculadora hace bien las sumas
+
+    Scenario Outline: "Cuando tengo dos numeros naturales haciendo el test con TS - FLOW"
+        Given TsFlow Dados dos numeros <numero_1> y <numero_2>
+        When TsFlow Cuando los sumo
+        Then TsFlow La suma es <resultado>
+
+        Examples:
+            | numero_1 | numero_2 | resultado |
+            | 1 | 25 | 26 |
+            | 2 | 26 | 28 |
+            | 3 | 27 | 30 |
+            | 4 | 28 | 32 |
+            | 5 | 29 | 34 |
+
