d27c0e3316cf59bbd85a64bf840bd75faedd9ba5
Alvaro Muñoz Garcia <amgarcia@arsys.es>
2021-10-08 07:47:36 +0200

    Añadiendo hooks

---
+++ test/features/step_definitions/hooks.ts
@@ -0,0 +1,25 @@
+import { BeforeAll, AfterAll, Before, After, BeforeStep, AfterStep } from "@cucumber/cucumber";
+
+BeforeAll( function() {
+    console.info( "Esto se ejecuta antes de todo" );
+} );
+
+AfterAll( function() {
+    console.info( "Esto se ejecuta despues de todo" );
+} );
+
+Before( function() {
+    console.info( "\tEsto se ejecuta antes del primer step de cada scenario" );
+} );
+
+After( function() {
+    console.info( "\tEsto se ejecuta despues del ultimo step de cada scenario" );
+} );
+
+BeforeStep( function() {
+    console.info( "\t\tEsto se ejecuta antes de cada step" );
+} );
+
+AfterStep( function() {
+    console.info( "\t\tEsto se ejecuta despues de cada step" );
+} );
